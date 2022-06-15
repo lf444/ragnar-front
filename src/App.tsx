facc146b9135b6fe8fae503ef17dcd6954afbad7
lf444 <lartin.fabien@gmail.com>
2022-06-15 10:43:09 +0200

    dev: push to /home

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,24 @@
-import React, { useEffect } from "react";
-import { Routes, Route, useNavigate } from "react-router-dom";
-import Home from "./components/home/Home";
-import RagnarRoute from "./components/RagnarRoute";
-
+import React, { useEffect } from 'react';
+import { Routes, Route, useNavigate } from 'react-router-dom';
+import Home from './components/home/Home';
+import RagnarRoute from './components/RagnarRoute';
 
 function App() {
   let navigate = useNavigate();
 
   useEffect(() => {
     // check if user is first time lauching the app and redirect to home
-    if (localStorage.getItem("firstTime") === null) {
-      localStorage.setItem("firstTime", "false");
-      //navigate("/home");
+    if (localStorage.getItem('firstTime') === null) {
+      localStorage.setItem('firstTime', 'false');
+      navigate('/home');
     }
   }, []);
 
   return (
     <React.Fragment>
       <Routes>
-        <Route path="/home" element={<Home />}></Route>
-        <Route path="*" element={<RagnarRoute />} />
+        <Route path='/home' element={<Home />}></Route>
+        <Route path='*' element={<RagnarRoute />} />
       </Routes>
     </React.Fragment>
   );
