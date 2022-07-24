e2beb7275433849bf6b8fa4ad77d139eadd30665
lf444 <lartin.fabien@gmail.com>
2022-07-24 22:25:46 +0200

    dev: add nft viewer

--- src/components/lock/MyNFT.tsx
+++ src/components/lock/MyNFT.tsx
@@ -7,6 +7,7 @@ import { useAccount } from "wagmi";
 import { Buffer } from "buffer";
 import { formatEther } from "ethers/lib/utils";
 import DisplayNFT from "./DisplayNFT";
+import { appLogger } from "../../utils/method";
 
 const MyNFT = () => {
   const { data, error } = useAccount();
@@ -36,6 +37,7 @@ const MyNFT = () => {
           method: "eth_requestAccounts",
         });
 <<<<<<< HEAD
+<<<<<<< HEAD
         const total = await lock.balanceOf(String(accounts));
         
         for (let i = 0; i < total; i++) {
@@ -53,25 +55,24 @@ const MyNFT = () => {
         for (let i = 0; i < +formatEther(total); i++) {
           nftByUser.push(await lock.tokenOfOwnerByIndex(accounts[0], i));
         }
+=======
+>>>>>>> 881e8eb (dev: add nft viewer)
 
-        nftByUser.forEach(async (element) =>
-          nftMetadataByUser.push(await lock.tokenURI(element))
-        );
-        let empty: any[] = [];
-        setTimeout(function () {
-          nftMetadataByUser.forEach(async (element) =>
-            empty.push(
-              ...test,
-              JSON.parse(
-                Buffer.from(
-                  nftMetadataByUser[0].substring(29),
-                  "base64"
-                ).toString()
-              )
-            )
-          );
-          setTest(empty);
-        }, 1000);
+        let emptyNFt: any[] = [];
+        const test = await lock.getNftsOfOwner(accounts[0]);
+        const allNFTOwned = await Promise.all(
+          test.map((e: any) => {
+            return lock.tokenURI(e);
+          })
+        )
+          .then((e) => {
+            emptyNFt = e.map((e: any) => {
+              return JSON.parse(
+                Buffer.from(e.substring(29), "base64").toString()
+              );
+            });
+          })
+          .then(() => setTest(emptyNFt));
       }
     } catch (error) {
       console.log(error);
